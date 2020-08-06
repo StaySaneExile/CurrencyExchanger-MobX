b002cf66cc5f9b7d8861ea697012daff20de54bc
StaySaneExile <vasiliy2490@gmail.com>
2020-08-06 16:31:11 +0500

    Updates

---
+++ precache-manifest.4596962a0afd426e93520b3a50e817ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07782e5308765be4bae157fbd538821f",
+    "url": "/CurrencyExchanger-MobX/index.html"
+  },
+  {
+    "revision": "4d619833465b294d7ca3",
+    "url": "/CurrencyExchanger-MobX/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ddcf752e9a873d055f3",
+    "url": "/CurrencyExchanger-MobX/static/js/2.9165ebe7.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/CurrencyExchanger-MobX/static/js/2.9165ebe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d619833465b294d7ca3",
+    "url": "/CurrencyExchanger-MobX/static/js/main.fce28a48.chunk.js"
+  },
+  {
+    "revision": "fa6da7ff337747c2a511",
+    "url": "/CurrencyExchanger-MobX/static/js/runtime-main.a469d911.js"
+  }
+]);
